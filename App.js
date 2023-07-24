0fc817580421a3f6090601c0aaff7e9a4b10ce40
xdKya <90745676+xdKya@users.noreply.github.com>
2023-07-24 16:40:54 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import DrawerNavigator from "./navigation/drawer";
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SpiderStory-Ultimate-Edition
+# Kya_StoryTelling-c88
+Aula c88 atualizada, completa e funcionando perfeitamente!
+
+AppLoading foi substituído por Splashscreen 
+
+adicionada uma imagem divertida para a imagem de perfil do usuário!
+
+funcionando na web e no smartphone.
